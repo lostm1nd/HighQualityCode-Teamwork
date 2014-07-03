0497a15a3d4d83d9234700699eb38c316044b9bf
simoto <sime679@gmail.com>
2014-07-03 16:58:20 +0300

    Particular implementation of Engine class
    the engine is not fully implementet

--- Minesweeper-7/Engine.cs
+++ Minesweeper-7/Engine.cs
@@ -8,10 +8,51 @@
     /// </summary>
     public class Engine: IEngine
     {
+        private IInputOutputManager renderer;
+        private IScoreBoard scoreBoard;
+        private Field field;
+        private bool playing;
+        private string currentUserInput;
+        public Engine()
+        {
+            this.renderer = new ConsoleInputOtputManager();
+            this.scoreBoard = new ScoreBoard();
+            this.field = new Field(5, 10);
+            this.playing = true;
+        }
         public void Play()
         {
             //TODO: Implement the main playing logic of the game.
+            renderer.PrintInitialMessage();
+            renderer.PrintGameField(this.field, false);
+
+            while (playing)
+            {
+                currentUserInput = renderer.GetUserInput();
+            }
+            
             throw new NotImplementedException();
+        }
+
+        private static void CheckCurrentCommand(string command)
+        {
+            //TODO: Implement this method
+            if (command.Length == 2)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                switch (command)
+                {
+                    case "exit": Environment.Exit(1); break;
+                    case "restart": throw new NotImplementedException();
+                    case "top": throw new NotImplementedException();
+                    default:
+                        break;
+                }
+            }
+            
         }
     }
 }
