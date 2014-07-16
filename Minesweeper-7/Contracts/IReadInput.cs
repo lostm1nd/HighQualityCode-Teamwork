d2f93d22a07071b8666a0dad71c4d91bc256da2f
d-donchev <ddonchev@gmail.com>
2014-07-16 06:17:05 +0300

    Interface changes

---
+++ Minesweeper-7/Contracts/IReadInput.cs
@@ -0,0 +1,22 @@
+﻿namespace Minesweeper.Contracts
+{
+    using System;
+
+    /// <summary>
+    /// Manages the input operations of the game.
+    /// </summary>
+    public interface IReadInput
+    {
+        /// <summary>
+        /// Gets the user nickname.
+        /// </summary>
+        /// <returns>Returns the user's nickname.</returns>
+        string GetUserNickname();
+
+        /// <summary>
+        /// Gets the user input - command or coordinates (row and column).
+        /// </summary>
+        /// <returns>Returns the user input.</returns>
+        string GetUserInput();
+    }
+}
